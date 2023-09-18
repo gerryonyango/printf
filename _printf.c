234a0524d01363025b2a5fa1c94c3ecf1caad27f
Wairimuu <xtynmaria@gmail.com>
2023-09-18 09:25:14 -0700

    done

--- _printf.c
+++ _printf.c
@@ -11,58 +11,32 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+	va_list args;
+	int i =0;
+	int printed_chars =0;
+	int (*func)(va_list args);
 
 	if (format == NULL)
-		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
+		return(-1)
+	va_start(args, format);
+	while(*(format +1)!= '\0'
 	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-					flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
+	if (*(format +1)!= '%')
+	{
+	printed_chars += _putchar(*(format+1))'
 	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
-}
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
-}
+	else if (*(format + i) == '%')
+	{
+	i++;
+	if (*(format+i) == 'c' || *(format + i) == 's' ||
+			*(format + i) == 'd' || *(format + i) == 'i')
+	{
+	else
+	printed_chars +=_putchar('%');
+	printed_chars +=_putchar(*(format + i));
+	}
+	i++;
+	}
+	va_end(args);
+	return(printed_chars);
+        
